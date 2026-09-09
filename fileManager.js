4e1f21db33d3d9b815c2162a457820fc8cbe2a49
akshit-88 <akshitff8@gmail.com>
2026-09-09 11:08:40 +0530

    Update fileManager.js

--- fileManager.js
+++ fileManager.js
@@ -1,11 +1,7 @@
-// file manager using fs module
-// run: node fileManager.js
-
 const fs = require("fs");
 
 const path = "./test.txt";
 
-// create
 console.log("Creating File...");
 fs.writeFile(path, "Hello Node.js", function (err) {
   if (err) {
@@ -14,7 +10,6 @@ fs.writeFile(path, "Hello Node.js", function (err) {
   }
   console.log("File Created");
 
-  // read
   console.log("Reading File");
   fs.readFile(path, "utf8", function (err, data) {
     if (err) {
@@ -23,7 +18,6 @@ fs.writeFile(path, "Hello Node.js", function (err) {
     }
     console.log(data);
 
-    // update
     fs.appendFile(path, "\nLearning FS Module", function (err) {
       if (err) {
         console.log("error updating file: " + err.message);
@@ -35,7 +29,6 @@ fs.writeFile(path, "Hello Node.js", function (err) {
         if (err) return;
         console.log(data);
 
-        // delete
         fs.unlink(path, function (err) {
           if (err) {
             console.log("error deleting file, maybe it doesn't exist: " + err.message);
